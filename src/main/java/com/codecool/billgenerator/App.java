86fbd984af7df491bd09a9fec0ef020adcb02e6e
Grelory <gmpwm99@gmail.com>
2020-10-07 09:56:43 +0200

    Implement main function to run a simple program

--- src/main/java/com/codecool/billgenerator/App.java
+++ src/main/java/com/codecool/billgenerator/App.java
@@ -1,8 +1,11 @@
 package com.codecool.billgenerator;
 
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        BillGenerator billGenerator = new BillGenerator();
+        billGenerator.loadItemsFromFile(args[0]);
+        billGenerator.loadBasketFromFile(args[1]);
+        billGenerator.generateBill();
+
     }
 }
